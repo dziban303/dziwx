1520885363483b31f53caa8ddc5a5619aab9660d
AWS CodeStar <noreply-awscodestar@amazon.com>
2018-05-26 01:39:35 +0000

    Initial commit made by AWS CodeStar during project creation.

---
+++ tests/test_handler.py
@@ -0,0 +1,17 @@
+import unittest
+import index
+
+
+class TestHandlerCase(unittest.TestCase):
+
+    def test_response(self):
+        print("testing response.")
+        result = index.handler(None, None)
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertIn('Hello World', result['body'])
+
+
+if __name__ == '__main__':
+    unittest.main()
