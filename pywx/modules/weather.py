29540e8a632b81448226f3c8acacc87ff57ad2e2
Dziban303 <dziban303@users.noreply.github.com>
2020-09-14 09:21:52 -0500

    Update weather.py
    hourly — command: hfx

--- pywx/modules/weather.py
+++ pywx/modules/weather.py
@@ -68,6 +68,9 @@ alert_colors = (
     ('special', 'null'),
 )
 temp_colors = ((-100, 'pink'), (15, 'pink'), (32, 'royal'), (50, 'teal'), (65, 'green'), (75, 'lime'), (85, 'orange'), (150, 'red'))
+dewpoint_colors = ((-100, 'royal'), (15, 'green'), (60, 'lime'), (65, 'yellow'), (70, 'orange'), (75, 'red'), (150, 'red'))
+
+spark_graph = list(u"▁▂▃▅▇")
 
 Airport = collections.namedtuple('Airport', 'airport_id name city country faa icao lat long alt tz dst')
 
@@ -87,6 +90,51 @@ def color_temp(ctx, temp):
     bold = True if ct > 100 else False
     return base.irc_color(pretty_temp(ctx, temp), color, bold=bold)
 
+@contextfilter
+def color_dewpoint(ctx, temp):
+    ct = int(to_fahrenheight(temp)) if ctx['units'].temp == 'C' else int(temp)
+    color = first_greater_selector(ct, dewpoint_colors)
+    bold = True if ct > 75 else False
+    return base.irc_color(pretty_temp(ctx, temp), color, bold=bold)
+
+@contextfilter
+def spark_temp(ctx, temps):
+    graph_line_selector = zip([min(temps)+((max(temps)-min(temps))/5.0*x) for x in range(5)], spark_graph) + [(max(temps)+1, spark_graph[-1])]
+    graph = []
+
+    for temp in temps:
+        ct = int(to_fahrenheight(temp)) if ctx['units'].temp == 'C' else int(temp)
+        color = first_greater_selector(ct, temp_colors)
+        bar = first_greater_selector(temp, graph_line_selector)
+        graph.append(base.irc_color(bar, color))
+    return ''.join(graph)
+
+@contextfilter
+def spark_dewpoint(ctx, temps):
+    graph_line_selector = zip([min(temps)+((max(temps)-min(temps))/5.0*x) for x in range(5)], spark_graph) + [(max(temps)+1, spark_graph[-1])]
+    graph = []
+
+    for temp in temps:
+        ct = int(to_fahrenheight(temp)) if ctx['units'].temp == 'C' else int(temp)
+        color = first_greater_selector(ct, dewpoint_colors)
+        bar = first_greater_selector(temp, graph_line_selector)
+        graph.append(base.irc_color(bar, color))
+    return ''.join(graph)
+
+@contextfilter
+def spark_precip(ctx, precips):
+    graph_line_selector = zip([0,0.1,0.3,0.6,0.8,1], spark_graph) + [(1, spark_graph[-1])]
+    graph = []
+
+    for precip in precips:
+        color = 'green' if precip[1] == 'rain' else 'purple' if precip[1] == 'snow' else 'aqua' if precip[1] == 'sleet' else 'white'
+        if precip[0] < 0.1:
+            color = 'white'
+        bar = first_greater_selector(precip[0], graph_line_selector)
+        graph.append(base.irc_color(bar, color))
+    return ''.join(graph)
+
+
 class BaseWeather(base.Command):
     def __init__(self, config):
         super(BaseWeather, self).__init__(config)
@@ -99,6 +147,10 @@ class BaseWeather(base.Command):
         super(BaseWeather, self).load_filters()
         self.environment.filters['temp'] = pretty_temp
         self.environment.filters['ctemp'] = color_temp
+        self.environment.filters['dptemp'] = color_dewpoint
+        self.environment.filters['spark_temp'] = spark_temp
+        self.environment.filters['spark_dewpoint'] = spark_dewpoint
+        self.environment.filters['spark_precip'] = spark_precip
         self.environment.filters['ic'] = lambda val, icon: base.irc_color(val, icon_colors[icon])
         self.environment.filters['icon_colors'] = icon_colors
         self.environment.filters['alert_colors'] = alert_colors
@@ -113,6 +165,8 @@ class BaseWeather(base.Command):
 
     def load_airports(self):
         airport_lookup = {}
+        # on mach5's new weather.py 
+        # adb = self.config.get('pywx_path') + '/airports.dat'
         adb = self.config.get('airportdb')
         if adb and os.path.exists(adb):
             for ap in csv.reader(open(adb)):
@@ -237,7 +291,7 @@ class WeatherForecast(BaseWeather):
 
 @register(commands=['hf',])
 class HourlyForecast(BaseWeather):
-    template = u"""
+    temp_template = u"""
         {{ name|nc }}:
         {% for hour in hourlies %}
             {{ (hour.time.strftime('%I')|int|string + hour.time.strftime('%p').lower())|c('maroon') }}:
@@ -245,8 +299,105 @@ class HourlyForecast(BaseWeather):
             {{ hour.temperature|ctemp }}
         {% endfor %}"""
 
+    wind_template = u"""
+        {{ name|nc }} {{ 'Winds'|nc }}:
+        {% for hour in hourlies %}
+            {{ (hour.time.strftime('%I')|int|string + hour.time.strftime('%p').lower())|c('maroon') }}:
+            {{ hour.windspeed }}
+            {% if hour.windgust %} {{ 'G'|c('red') }}: {{ hour.windgust }} {% endif %}
+        {% endfor %}"""
+
+    dewpoint_template = u"""
+        {{ name|nc }} {{ 'Dewpoints'|nc }}:
+        {% for hour in hourlies %}
+            {{ (hour.time.strftime('%I')|int|string + hour.time.strftime('%p').lower())|c('maroon') }}:
+            {{ hour.dewpoint|dptemp }}
+        {% endfor %}"""
+
+    precip_template = u"""
+        {{ name|nc }} {{ 'Precip'|nc }}:
+        {% for hour in hourlies %}
+            {{ (hour.time.strftime('%I')|int|string + hour.time.strftime('%p').lower())|c('maroon') }}:
+            {% if hour.precip_type %} {{ hour.precip_type|ic(hour.icon) }} ({{ hour.precip_prob }}%) {% else %} - {% endif %}
+        {% endfor %}"""
+
+    def parse_args(self, msg):
+        parser = base.IRCArgumentParser()
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-F', action="store_true")
+        group.add_argument('-C', action="store_true")
+        group.add_argument('-d', action="store_true")
+        group.add_argument('-w', action="store_true")
+        group.add_argument('-p', action="store_true")
+        parser.add_argument('location', type=str, default=None, nargs='*')
+        return parser.parse_args(msg)
+
     def context(self, msg):
         payload = super(HourlyForecast, self).context(msg)
+        forecast = payload['forecast']
+        units = payload['units']
+        timezone = pytz.timezone(forecast.json['timezone'])
+
+        self.template = self.precip_template if payload['args'].p else self.temp_template
+        self.template = self.dewpoint_template if payload['args'].d else self.template
+        self.template = self.wind_template if payload['args'].w else self.template
+
+        hourly = forecast.hourly().data[:12]
+        hourlies = []
+        for h in hourly:
+            hour = {
+               'time': pytz.utc.localize(h.time).astimezone(timezone),
+               'icon': h.icon,
+               'summary': h.summary,
+               'temperature': h.temperature,
+               'dewpoint': h.dewPoint,
+            }
+
+            if h.precipProbability > 0:
+                hour['precip_prob'] = int(h.precipProbability*100)
+                hour['precip_type'] = h.precipType.title()
+            else:
+                hour['precip_prob'] = 0
+                hour['precip_type'] = False
+
+            windspeed = h.windSpeed if forecast.json['flags']['units'] != 'si' else h.windSpeed*3.6 #convert m/s to kph
+            hour['windspeed'] = '%s%s %s' % (int(windspeed), units.wind, first_greater_selector(h.windBearing, wind_directions))
+            if h.windGust > 20 and units.wind == 'mph':
+                hour['windgust'] = int(round(h.windGust))
+            elif h.windGust > 32 and units.wind == 'kph':
+                hour['windgust'] = int(round(h.windGust))
+            elif h.windGust > 8 and units.wind == 'm/s':
+                hour['windgust'] = round(h.windGust, 1)
+            else:
+                hour['windgust'] = None
+
+            if hour['windgust']:
+                hour['windgust'] = "{}{}".format(hour['windgust'], units.wind)
+            hourlies.append(hour)
+
+        payload['hourlies'] = hourlies
+        return payload
+
+
+@register(commands=['hfx',])
+class HourlySparkForecast(BaseWeather):
+    template = u"""
+        {{ name|nc }}:
+        {{ "Temp"|c('maroon') }}: {{ temps|spark_temp }}
+        {{ "Dewpoint"|c('maroon') }}: {{ dewpoints|spark_dewpoint }}
+        {{ "Precip"|c('maroon') }}: {{ precip|spark_precip }}
+        """
+
+    def parse_args(self, msg):
+        parser = base.IRCArgumentParser()
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-F', action="store_true")
+        group.add_argument('-C', action="store_true")
+        parser.add_argument('location', type=str, default=None, nargs='*')
+        return parser.parse_args(msg)
+
+    def context(self, msg):
+        payload = super(HourlySparkForecast, self).context(msg)
         forecast = payload['forecast']
         units = payload['units']
         timezone = pytz.timezone(forecast.json['timezone'])
@@ -259,11 +410,24 @@ class HourlyForecast(BaseWeather):
                'icon': h.icon,
                'summary': h.summary,
                'temperature': h.temperature,
+               'dewpoint': h.dewPoint,
             }
-            hourlies.append(hour)
-        payload['hourlies'] = hourlies
-        return payload
 
+            if h.precipProbability > 0:
+                hour['precip_prob'] = h.precipProbability
+                hour['precip_type'] = h.precipType
+            else:
+                hour['precip_prob'] = 0
+                hour['precip_type'] = False
+
+            windspeed = h.windSpeed if forecast.json['flags']['units'] != 'si' else h.windSpeed*3.6 #convert m/s to kph
+            hour['windspeed'] = '%s%s %s' % (int(windspeed), units.wind, first_greater_selector(h.windBearing, wind_directions))
+            hourlies.append(hour)
+
+        payload['temps'] = [x['temperature'] for x in hourlies]
+        payload['dewpoints'] = [x['dewpoint'] for x in hourlies]
+        payload['precip'] = [(x['precip_prob'], x['precip_type']) for x in hourlies]
+        return payload
 
 
 @register(commands=['wx',])
@@ -441,7 +605,7 @@ class Alert(BaseWeather):
 class Locate(BaseWeather):
     # elevation_api = "https://maps.googleapis.com/maps/api/elevation/json"
     # template = "{{ name|nc }}: {{ lat }}, {{ lng }} {{ 'Elevation'|tc }}: {{ elevation|int }}m ({{ elevation_ft|int }}ft)"
-    template = "{{ name|nc }}: {{ lat }}, {{ lng }}"
+    # template = "{{ name|nc }}: {{ lat }}, {{ lng }}"
 
     # def get_elevation(self, latlng):
         # try:
